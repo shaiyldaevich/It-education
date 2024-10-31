3fa44a07e287e132dbbd17226fbb83efa32fe97d
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2024-10-31 14:39:40 +0600

    lol

--- src/app/(site)/article/page.tsx
+++ src/app/(site)/article/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import LastStates from "@/AppPages/site/pages/homeSections/LastState/LastState";
+import React from "react";
 
 const page = () => {
-    return (
-        <div>
-            article
-        </div>
-    );
+  return (
+    <div>
+      article
+      <LastStates />
+    </div>
+  );
 };
 
 export default page;
