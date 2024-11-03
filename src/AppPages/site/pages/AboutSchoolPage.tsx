ffc9ad1a842e04bc27f3742f98a32d406247c7c3
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2024-11-03 11:47:34 +0600

    lol

--- src/AppPages/site/pages/AboutSchoolPage.tsx
+++ src/AppPages/site/pages/AboutSchoolPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LevelOf from './AboutSection/LevelOf/LevelOf';
 
 const AboutSchoolPage = () => {
     return (
         <div>
-            
+            <LevelOf/>
         </div>
     );
 };
