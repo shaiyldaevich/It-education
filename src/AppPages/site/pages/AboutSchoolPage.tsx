28439e2c00fca5f47d62a9da7da617bd774c3103
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2024-11-04 15:44:48 +0600

    lol

--- src/AppPages/site/pages/AboutSchoolPage.tsx
+++ src/AppPages/site/pages/AboutSchoolPage.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import LevelOf from "./AboutSection/LevelOf/LevelOf";
 import MasterClass from "./AboutSection/MasterClass/MasterClass";
 import AboutSchool from "./AboutSection/AboutSchool/AboutSchool";
+import Request from "./homeSections/Request/Request";
 
 const AboutSchoolPage = () => {
-    return (
-        <div>
-            <AboutSchool />
-            <MasterClass />
-            <LevelOf />
-        </div>
-    );
+  return (
+    <div>
+      <AboutSchool />
+      <MasterClass />
+      <LevelOf />
+      <Request />
+    </div>
+  );
 };
 
 export default AboutSchoolPage;
