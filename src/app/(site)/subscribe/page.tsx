731c12728d7657c5a450ec9296bc84495ccbca41
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-11-08 20:07:15 +0600

    added subsc pag

---
+++ src/app/(site)/subscribe/page.tsx
@@ -0,0 +1,12 @@
+import SubscribePage from "@/AppPages/site/pages/SubscribePage";
+import React from "react";
+
+const page = () => {
+    return (
+        <div>
+            <SubscribePage />
+        </div>
+    );
+};
+
+export default page;
