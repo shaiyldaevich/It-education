0ecadaee8615559e3c9ad14a80b339388b67f831
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2024-11-12 12:49:39 +0600

    lol

--- src/AppPages/site/pages/AboutSection/MasterClass/DetMasterClass/WhoMove/WhoMasterClass.tsx
+++ src/AppPages/site/pages/AboutSection/MasterClass/DetMasterClass/WhoMove/WhoMasterClass.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import scss from "./WhoMasterClass.module.scss";
 import Image from "next/image";
 import Master from "@/assets/master.svg";
+import { useLanguageStore } from "@/stores/useLanguageStore";
 
 const WhoMasterClass = () => {
-    return (
-        <div className={scss.WhoMasterClass}>
-            <div className="container">
-                <div className={scss.content}>
-                    <Image src={Master} alt="master" />
-                    <h3>Мастер класс ведет</h3>
-                    <h1>Евгений Александрович</h1>
-                    <h4> Frontend developer / Аратор </h4>
-                </div>
-            </div>
+  const { t } = useLanguageStore();
+  return (
+    <div className={scss.WhoMasterClass}>
+      <div className="container">
+        <div className={scss.content}>
+          <Image src={Master} alt="master" />
+          <h3>{t("Мастер классты откозот", "Мастер класс ведет")}</h3>
+          <h1>Евгений Александрович</h1>
+          <h4> Frontend developer / Аратор </h4>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WhoMasterClass;
